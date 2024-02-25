9cfc496141ae76f959cf75dc54d5140f7150ca7b
Korawan <korawan.ta@ku.th>
2024-02-25 19:40:14 +0700

    Routes AddRoutes User

--- user/urls.py
+++ user/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import api
 from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 # from .views import UserViewSet
 
 router = DefaultRouter()
 # router.register(r'users', UserViewSet)
+router.register(r'user', UserViewSet)
 
 urlpatterns = [
     # path("me/", api.me, name="me"), 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User
+from .serializer import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    
+    
+
 
 # Create your views here.
